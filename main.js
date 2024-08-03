f462d41f982b0944b43d4a3a1cdb51e58ed31010
seoyeom542 <seoyeom542@gmail.com>
2024-08-03 20:08:21 +0900

    to-currency-list click event 추가

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let currencyRatio = {
   KRW: { KRW: 1, USD: 0.00074, VND: 18.57, unit: "원" },
   VND: { KRW: 0.054, USD: 0.00004, VND: 1, unit: "동" },
 };
+let fromCUrrency = "USD";
+let toCurrency = "USD";
 
 // console.log(currencyRatio.KRW.unit);
 // console.log(currencyRatio["KRW"]["unit"]);
@@ -10,5 +12,13 @@ let currencyRatio = {
 document.querySelectorAll("#from-currency-list a").forEach((menu) =>
   menu.addEventListener("click", function () {
     document.getElementById("from-button").textContent = this.textContent;
+    fromCUrrency = this.textContent;
   })
 );
+
+document.querySelectorAll("#to-currency-list a").forEach((menu) => {
+  menu.addEventListener("click", function () {
+    document.getElementById("to-button").textContent = this.textContent;
+    toCurrency = this.textContent;
+  });
+});
