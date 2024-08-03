28d66c08a1bc6e2b2a7b1e4adea85d48a7c0ab40
seoyeom542 <seoyeom542@gmail.com>
2024-08-03 19:45:19 +0900

    화폐 단위 추가

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let currencyRatio = {
-  USD: { KRW: 1358.16, USD: 1, VND: 25217.5 },
-  KRW: { KRW: 1, USD: 0.00074, VND: 18.57 },
-  VND: { KRW: 0.054, USD: 0.00004, VND: 1 },
+  USD: { KRW: 1358.16, USD: 1, VND: 25217.5, unit: "달러" },
+  KRW: { KRW: 1, USD: 0.00074, VND: 18.57, unit: "원" },
+  VND: { KRW: 0.054, USD: 0.00004, VND: 1, unit: "동" },
 };
