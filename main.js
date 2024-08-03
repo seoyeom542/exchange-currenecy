8ee3843501647659914895e0328db18ecbbb0888
seoyeom542 <seoyeom542@gmail.com>
2024-08-03 20:04:11 +0900

    click event 추가

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let currencyRatio = {
   KRW: { KRW: 1, USD: 0.00074, VND: 18.57, unit: "원" },
   VND: { KRW: 0.054, USD: 0.00004, VND: 1, unit: "동" },
 };
+
+// console.log(currencyRatio.KRW.unit);
+// console.log(currencyRatio["KRW"]["unit"]);
+
+document.querySelectorAll("#from-currency-list a").forEach((menu) =>
+  menu.addEventListener("click", function () {
+    document.getElementById("from-button").textContent = this.textContent;
+  })
+);
